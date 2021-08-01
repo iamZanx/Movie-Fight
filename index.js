5c449530d9322c2eaad9d7c26f9923b75e59439b
iamZanx <54510834+iamZanx@users.noreply.github.com>
2021-08-01 17:51:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
+const debounce = (func, delay = 1000) => {
+    let timeoutID;
+    return (...args) => {
+        if(timeoutID){
+            clearInterval(timeoutID);
+        }
+        timeoutID = setTimeout(() => {
+            func.apply(null, args)
+        }, delay);
+    };
+};
+
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
+	root.innerHTML = `
+    <label><strong>Search for a item</strong></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+	const input = root.querySelector('input');
+	const dropdown = root.querySelector('.dropdown');
+	const resulstWrapper = root.querySelector('.results');
+
+	const onInput = async (event) => {
+		const items = await fetchData(event.target.value);
+
+		if (!items.length) {
+			dropdown.classList.remove('is-active');
+			return;
+		}
+
+		resulstWrapper.innerHTML = '';
+		dropdown.classList.add('is-active');
+
+		for (let item of items) {
+			const option = document.createElement('a');
+
+			option.classList.add('dropdown-item');
+			option.innerHTML = renderOption(item);
+
+			option.addEventListener('click', () => {
+				input.value = inputValue(item);
+				dropdown.classList.remove('is-active');
+				onOptionSelect(item);
+			});
+
+			resulstWrapper.appendChild(option);
+		}
+	};
+	input.addEventListener('input', debounce(onInput, 500));
+
+	document.addEventListener('click', (event) => {
+		if (!root.contains(event.target)) {
+			dropdown.classList.remove('is-active');
+		}
+	});
+};
+
 const autoCompleteConfig = {
 	renderOption(movie) {
 		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
